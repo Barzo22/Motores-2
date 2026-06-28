150595b92a4523c55a5d22daf50b26d88e61a2ef
Barzo <barzobruno11@gmail.com>
2026-06-28 01:19:14 -0300

    arte

--- Assets/Scripts/Items/Door.cs
+++ Assets/Scripts/Items/Door.cs
@@ -2,19 +2,44 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] Sprite openSprite;
+    [SerializeField] Sprite closedSprite;
+
+    SpriteRenderer spriteRenderer;
+    Collider2D col;
+    bool isOpen = false;
+
     string doorID;
 
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+
         doorID = $"door_{transform.position.x}_{transform.position.y}";
 
         if (GameManager.Instance.IsDoorOpen(doorID))
-            gameObject.SetActive(false);
+            SetOpen(true);
+        else
+            SetOpen(false);
     }
 
     public void Open()
     {
         GameManager.Instance.RegisterDoorOpen(doorID);
-        gameObject.SetActive(false);
+        SetOpen(true);
+    }
+
+    void SetOpen(bool open)
+    {
+        isOpen = open;
+        spriteRenderer.sprite = open ? openSprite : closedSprite;
+        col.enabled = open;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && isOpen)
+            GameManager.Instance.LevelComplete(GameManager.Instance.GetCoinsCollectedThisAttempt());
     }
 }
